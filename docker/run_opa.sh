447db188db8134de3a76534c8a04ca025b3dae69
Dima <dmitry.korolev@gmail.com>
2022-06-10 11:41:36 -0500

    Added `gengolden` as a command to the Docker container as well.

--- docker/run_opa.sh
+++ docker/run_opa.sh
@@ -9,6 +9,23 @@ if [ "$1" == "ir" ] ; then
     echo 'Easiest way to obtain `CONTAINER_ID`: `export CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` repo.'
     exit 1
   fi
+elif [ "$1" == "gengolden" ] ; then
+  if [ $# == 5 ] ; then
+    opa build /input/"$2"
+    opa run --server bundle.tar.gz -l error >/dev/null 2>dev/null &
+    OPA_PID=$!
+    sleep 0.5  # TODO(dkorolev): I would love to check `localhots:8181/health`, but it just returns `{}`, w/o HTTP code or body.
+    while read -r QUERY ; do
+      curl -s -d "{\"input\":$QUERY}" localhost:8181/v1/data | sed 's/^{"result"://' | sed 's/}$//' ; echo
+    done < /input/"$5"
+    kill $OPA_PID
+    wait
+  else
+    echo 'Recommended synopsis: `docker run -v $PWD:/input $CONTAINER_ID gengolden policy.rego myapi result tests.json`.'
+    echo 'This requires `policy.rego` and `tests.json` in the current directory. The tests should be one JSON per line.'
+    echo 'Easiest way to obtain `CONTAINER_ID`: `export CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` repo.'
+    exit 1
+  fi
 else
   opa $*
 fi
