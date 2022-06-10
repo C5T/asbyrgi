74f18dac8990f1fb01d8d6d133e0d6faafdf7806
Dima <dmitry.korolev@gmail.com>
2022-06-10 11:04:48 -0500

    Added IR extraction as a "container" command.

--- docker/run_opa.sh
+++ docker/run_opa.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 if [ "$1" == "ir" ] ; then
-  echo "Here be dragons."
+  if [ $# == 4 ] ; then
+    opa build /input/"$2" -e "$3"/"$4" -t plan -o /dev/stdout | tar xz -O /plan.json 2>/dev/null
+  else
+    echo 'Recommended synopsis: `docker run -v $PWD:/input $CONTAINER_ID ir policy.rego myapi result | jq .`.'
+    echo 'This requires `policy.rego` in the current directory, and outputs the IR for package `myapi`, rule `result`, as a JSON.'
+    echo 'Easiest way to obtain `CONTAINER_ID`: `export CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` repo.'
+    exit 1
+  fi
 else
   opa $*
 fi
