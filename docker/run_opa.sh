ae330a96ee3ba4cfddf5b936659dab4fdd7b27c2
Dima <dmitry.korolev@gmail.com>
2022-06-12 12:39:45 -0500

    Made the JSOPA Docker container more stdin-friendly (no more `-v`-s).

--- docker/run_opa.sh
+++ docker/run_opa.sh
@@ -4,29 +4,21 @@
 #                 It does work outside that container too if you have OPA installed :-) but that's not the intended usage.
 
 if [ "$1" == "rego2ir" ] ; then
-  if [ $# == 4 ] ; then
-    if ! [ -e /input/"$2" ] ; then
-      echo "The input '$2' file is not found."
-      exit 1
-    fi
-    if ! opa build /input/"$2" -e "$3"/"$4" -t plan -o /dev/stdout | tar xz -O /plan.json 2>/dev/null ; then
+  if [ $# == 3 ] ; then
+    if ! opa build /dev/stdin -e "$2"/"$3" -t plan -o /dev/stdout | tar xz -O /plan.json 2>/dev/null ; then
       echo 'OPA run failed.' >/dev/stderr
       exit 1
     fi
     exit 0
   else
-    echo 'Recommended synopsis: `docker run -v "$PWD"/input $JSOPA_CONTAINER_ID rego2ir policy.rego myapi result | jq .`.'
-    echo 'This requires `policy.rego` in the current directory, and outputs the IR for package `myapi`, rule `result`, as a JSON.'
+    echo 'Usage: cat policy.rego | docker run -i $JSOPA_CONTAINER_ID rego2ir myapi result | jq .'
+    echo 'The above command would generate the IR for the rule `result` from the package `myapi` of `policy.rego`.'
     echo 'Easiest way to obtain `JSOPA_CONTAINER_ID`: `export JSOPA_CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` dir.'
     exit 1
   fi
 elif [ "$1" == "rego2dsl" ] ; then
-  if [ $# == 4 ] ; then
-    if ! [ -e /input/"$2" ] ; then
-      echo "The input '$2' file is not found."
-      exit 1
-    fi
-    if ! opa build /input/"$2" -e "$3"/"$4" -t plan -o /dev/stdout | tar xz -O /plan.json >/tmp/ir.json 2>/dev/null ; then
+  if [ $# == 3 ] ; then
+    if ! opa build /dev/stdin -e "$2"/"$3" -t plan -o /dev/stdout | tar xz -O /plan.json >/tmp/ir.json 2>/dev/null ; then
       echo 'OPA run failed.' >/dev/stderr
       exit 1
     fi
@@ -34,23 +26,19 @@ elif [ "$1" == "rego2dsl" ] ; then
       echo 'No IR generated.' >/dev/stderr
       exit 1
     fi
-    # TODO(dkorolev): Pass "$3" and "$4" to the script.
+    # TODO(dkorolev): Pass "$2" and "$3" to the script.
     /src/ir2dsl.js /tmp/ir.json
     rm -f /tmp/ir.json
     exit 0
   else
-    echo 'Recommended synopsis: `docker run -v "$PWD"/input $JSOPA_CONTAINER_ID rego2dsl policy.rego myapi result`.'
-    echo 'This requires `policy.rego` in the current directory, and outputs the IR in the DSL format for it.'
+    echo 'Usage: cat policy.rego | docker run -i $JSOPA_CONTAINER_ID rego2dsl myapi result'
+    echo 'The above command would generate the DSL for the rule `result` from the package `myapi` of `policy.rego`.'
     echo 'Easiest way to obtain `JSOPA_CONTAINER_ID`: `export JSOPA_CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` dir.'
     exit 1
   fi
 elif [ "$1" == "rego2js" ] ; then
-  if [ $# == 4 ] ; then
-    if ! [ -e /input/"$2" ] ; then
-      echo "The input '$2' file is not found."
-      exit 1
-    fi
-    if ! opa build /input/"$2" -e "$3"/"$4" -t plan -o /dev/stdout | tar xz -O /plan.json >/tmp/ir.json 2>/dev/null ; then
+  if [ $# == 3 ] ; then
+    if ! opa build /dev/stdin -e "$2"/"$3" -t plan -o /dev/stdout | tar xz -O /plan.json >/tmp/ir.json 2>/dev/null ; then
       echo 'OPA run failed.' >/dev/stderr
       exit 1
     fi
@@ -58,13 +46,13 @@ elif [ "$1" == "rego2js" ] ; then
       echo 'No IR generated.' >/dev/stderr
       exit 1
     fi
-    # TODO(dkorolev): Pass "$3" and "$4" to the script.
+    # TODO(dkorolev): Pass "$2" and "$3" to the script.
     (cat /src/preprocess.inl.js; /src/ir2dsl.js /tmp/ir.json) | cpp | grep -v '^#' | grep -v '^$'
     rm -f /tmp/ir.json
     exit 0
   else
-    echo 'Recommended synopsis: `docker run -v "$PWD"/input $JSOPA_CONTAINER_ID rego2js policy.rego myapi result`.'
-    echo 'This requires `policy.rego` in the current directory, and outputs a standalone, runnable JavaScript source file for it.'
+    echo 'Usage: cat policy.rego | docker run -i $JSOPA_CONTAINER_ID rego2js myapi result'
+    echo 'The above command would generate the JavaScript for the rule `result` from the package `myapi` of `policy.rego`.'
     echo 'Easiest way to obtain `JSOPA_CONTAINER_ID`: `export JSOPA_CONTAINER_ID=$(docker build -q .)` from the `jsopa/docker` dir.'
     exit 1
   fi
