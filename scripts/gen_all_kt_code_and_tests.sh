0739a155504e500f7eb4323c827d116341117975
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-09-06 21:23:52 +0400

    And even more [blind] tests.

--- scripts/gen_all_kt_code_and_tests.sh
+++ scripts/gen_all_kt_code_and_tests.sh
@@ -16,30 +16,17 @@ for REGO_TEST_CASE in $(find tests/ -iname '*.rego' | sort); do
     cp $REGO_TEST_CASE.kt kt_test/src/main/kotlin/$REGO_KT_IMPL_NAME.kt
 
     HEADER=$(head -n 1 "$REGO_TEST_CASE")
-    TMPFILE=$(mktemp)
     if [[ "$HEADER" =~ \#!TEST ]] ; then
       read -r UNUNSED_TEST PACKAGE RULE <<< "$HEADER"
-
-      echo "CHECKPOINT 1: $(wc -l "$(dirname "$REGO_TEST_CASE")/tests.json")"
-
       while IFS= read -r line; do
-        echo "CHECKPOINT 2: $line"
-        echo "$line" >$TMPFILE
-        docker run -i $CONTAINER_ID eval --data "$REGO_TEST_CASE" --input $TMPFILE data.$PACKAGE.$RULE
-      done < "$(dirname "$REGO_TEST_CASE")/tests.json"
-
-      while IFS= read -r line; do
-        echo "CHECKPOINT 3: $line"
-        echo "$line" >$TMPFILE
         echo -en "$line\t"
-        docker run -i $CONTAINER_ID eval --data "$REGO_TEST_CASE" --input $TMPFILE data.$PACKAGE.$RULE \
+        echo "$line" \
+        | docker run -v "$PWD/$(dirname "$REGO_TEST_CASE")":/$(dirname "$REGO_TEST_CASE") -i $CONTAINER_ID eval --data "$REGO_TEST_CASE" --input /dev/stdin data.$PACKAGE.$RULE \
         | jq .result[0].expressions[0].value
       done < "$(dirname "$REGO_TEST_CASE")/tests.json" | tr - - \
       | node tests/compose_kt_test.js $REGO_KT_IMPL_NAME \
       > tee kt_test/src/test/kotlin/${REGO_KT_IMPL_NAME}Test.kt
     fi
-
-    rm $TMPFILE
   else
     echo "Skipping '$REGO_TEST_CASE' because it has no 'kotlin_class_name'."
   fi
