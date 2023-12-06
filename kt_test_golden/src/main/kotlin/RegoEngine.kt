556a5e73576a9625454112a3e5f6b57e246abada
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-12-05 14:58:30 -1000

    Added more data provider injectors.

--- kt_test_golden/src/main/kotlin/RegoEngine.kt
+++ kt_test_golden/src/main/kotlin/RegoEngine.kt
@@ -392,9 +392,23 @@ class AuthzDataProvider {
         return this
     }
 
+    fun injectBoolean(path: String, value: Boolean): AuthzDataProvider {
+        fun helper(): AuthzValue = AuthzValue.BOOLEAN(value)
+        paths[path] = ::helper
+        return this
+    }
+
     fun injectInt(path: String, cb: () -> Int): AuthzDataProvider {
         fun helper(): AuthzValue {
             return AuthzValue.INT(cb())
+        }
+        paths[path] = ::helper
+        return this
+    }
+
+    fun injectInt(path: String, value: Int): AuthzDataProvider {
+        fun helper(): AuthzValue {
+            return AuthzValue.INT(value)
         }
         paths[path] = ::helper
         return this
@@ -408,10 +422,33 @@ class AuthzDataProvider {
         return this
     }
 
-    // TODO(dkorolev): More injectors!
+    fun injectString(path: String, value: String): AuthzDataProvider {
+        fun helper(): AuthzValue {
+            return AuthzValue.STRING(value)
+        }
+        paths[path] = ::helper
+        return this
+    }
+
 
     fun injectValue(path: String, cb: () -> AuthzValue): AuthzDataProvider {
         paths[path] = cb
+        return this
+    }
+
+    fun injectValue(path: String, value: AuthzValue): AuthzDataProvider {
+        fun helper(): AuthzValue {
+            return value
+        }
+        paths[path] = ::helper
+        return this
+    }
+
+    fun injectNull(path: String): AuthzDataProvider {
+        fun helper(): AuthzValue {
+            return AuthzValue.NULL
+        }
+        paths[path] = helper
         return this
     }
 
